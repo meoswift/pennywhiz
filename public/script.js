47e1899fed581a227ce15cba484b6f1ff3f8df58
Linh Tran <linh.tran@tufts.edu>
2019-11-27 02:06:02 -0500

    hide api key

--- public/script.js
+++ public/script.js
@@ -1,13 +1,14 @@
 var firebaseConfig = {
-    apiKey: config.apiKey,
+    apiKey: config.key || process.env.key,
     authDomain: "pennywhiz-49ed0.firebaseapp.com",
     databaseURL: "https://pennywhiz-49ed0.firebaseio.com",
     projectId: "pennywhiz-49ed0",
     storageBucket: "pennywhiz-49ed0.appspot.com",
-    messagingSenderId: config.messagingSenderId,
-    appId: config.appId,
-    measurementId: config.measurementId
+    messagingSenderId: "337268457584",
+    appId: "1:337268457584:web:d7c1950702b3d2741f34b5",
+    measurementId: "G-HZMF81JZD0"
   };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
