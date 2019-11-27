857dceee17752b78a54e53b984ba2f026e4aee14
Linh Tran <linh.tran@tufts.edu>
2019-11-27 02:32:32 -0500

    /api keys added back

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 var firebaseConfig = {
-    apiKey: config.key || process.env.key,
+    apiKey: "AIzaSyBI7YW8WHkQn6Mvp7bjt6O0fGjiPhxVv6A",
     authDomain: "pennywhiz-49ed0.firebaseapp.com",
     databaseURL: "https://pennywhiz-49ed0.firebaseio.com",
     projectId: "pennywhiz-49ed0",
@@ -7,7 +7,7 @@ var firebaseConfig = {
     messagingSenderId: "337268457584",
     appId: "1:337268457584:web:d7c1950702b3d2741f34b5",
     measurementId: "G-HZMF81JZD0"
-  };
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
