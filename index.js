ad8108cf8497af4fe1ae583954733e85007433ac
gmelch <54954041+gmelch@users.noreply.github.com>
2019-12-11 11:36:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 5000
 
+
+// MONGO CONNECTION
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://gmelcher:131313A@ds253428.mlab.com:53428/heroku_k05ffvws";
+MongoClient.connect(uri, function (err, client) {
+  // perform actions on the collection object
+  client.close();
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
