e59c07d995f10e30207ee137031933c2c5f21181
erindgb <54953912+erindgb@users.noreply.github.com>
2019-12-12 11:45:29 -0500

    Update chart.js

--- public/chart.js
+++ public/chart.js
@@ -1,12 +1,6 @@
-    let myChart = document.getElementById('myChart').getContext('2d');
-
-    Chart.defaults.global.defaultFontFamily = 'Helvetica';
-    Chart.defaults.global.defaultFontSize = 20;
-    Chart.defaults.global.defaultFontColor = 'grey';
-
-    var spentData = [];
-    var remainData = [];
-    var labelsCategory = [];
+		var spentData = [];
+		var remainData = [];
+		var labelsCategory = [];
 
 		spentData.push('3240');
 		remainData.push('4000');
@@ -19,9 +13,8 @@
 		spentData.push('1980');
 		remainData.push('500');
 		labelsCategory.push('Food');
- 
 
-    var barChartData = {
+		var barChartData = {
 			labels: labelsCategory,
 			datasets: [{
 				label: 'Spent',
@@ -34,52 +27,38 @@
 			}]
 		};
 
-    let myChart = new Chart(myChart, {
-      type:'bar',
-      data: barChartData,
-      options:{
-        title: {
-          display: true,
-          text: 'Budget Overview',
-          fontSize: 30,
-          padding: 30
-        },
-        legend: {
-          position: 'right',
-          display: false
-        },
-        layout: {
-          padding: 20
-        },
-        tooltips: {
-          enabled: true,
-          mode: 'index',
-          intersect: false
-        },
-        responsive: true,
-        scales: {
-          xAxes: [{
-            stacked: true,
-          }],
-          yAxes: [{
-            stacked: true
-          }]
-        }
-    }
-    });
+		window.onload = function() {
+			var ctx = document.getElementById('myChart').getContext('2d');
 
-    function addData(chart, label, data) {
-      chart.data.labels.push(label);
-      chart.data.datasets.forEach((dataset) => {
-          dataset.data.push(data);
-        });
-      chart.update();
-    }
+			Chart.defaults.global.defaultFontSize = 20;
+			Chart.defaults.global.defaultFontFamily = 'Helvetica';
 
-    function removeData(chart) {
-      chart.data.labels.pop();
-      chart.data.datasets.forEach((dataset) => {
-          dataset.data.pop();
-        });
-      chart.update();
-    }
+			window.myBar = new Chart(ctx, {
+				type: 'bar',
+				data: barChartData,
+				options: {
+					title: {
+						display: true,
+						text: 'Budget Overview',
+						fontSize: 30,
+	          padding: 30
+					},
+					legend: {
+						display: false
+					},
+					tooltips: {
+						mode: 'index',
+						intersect: false
+					},
+					responsive: true,
+					scales: {
+						xAxes: [{
+							stacked: true,
+						}],
+						yAxes: [{
+							stacked: true
+						}]
+					}
+				}
+			});
+		};
