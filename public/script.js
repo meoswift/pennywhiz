5d749c9417673b6dca854828c49c581c723909f4
Linh Tran <linh.tran@tufts.edu>
2019-12-11 22:32:40 -0500

    Finished Planned and Spent

--- public/script.js
+++ public/script.js
@@ -1,11 +1,33 @@
+// Selecting DOM elements from HTML file to work with
 const sidebarList = document.querySelectorAll('#sidebar li button')
 const sidebar = document.querySelector('#sidebar')
 
+// Elements in Planned tab
+const budgetContainer = document.getElementById('budget-container')
+const budgetTemplate = document.getElementById("budget-template")
+const newBudgetForm = document.getElementById('planned-form')
+const newBudgetCategory = document.querySelector('[data-new-category]')
+const newBudgetAmount = document.querySelector('[data-new-budget]')
+
+// Elements in Spent tab
+const itemsContainer = document.getElementById('items-container')
+const itemTemplate = document.getElementById('item-template')
+const newItemForm = document.getElementById('spent-form')
+const selectBox = document.getElementById('select-box')
+const newDescription = document.querySelector('[data-new-description]')
+const newPrice = document.querySelector('[data-new-price]')
+const categoryOptions = document.querySelector('[data-category-options')
+
+// Use local storage to store which tab the user is on - store key and get items
 const LOCAL_STORAGE_SELECTED_TAB_ID_KEY = 'tab.selectedId'
-let selectedTabId = localStorage.getItem(LOCAL_STORAGE_SELECTED_TAB_ID_KEY)
+let selectedTabId = localStorage.getItem(LOCAL_STORAGE_SELECTED_TAB_ID_KEY) || 'planned'
 
-const list = []
+// List of budgets - inside each budget category is a list of items spent
+const LOCAL_STORAGE_BUDGET_LIST_ID_KEY = 'budgets.id'
+let budgetList = JSON.parse(localStorage.getItem(LOCAL_STORAGE_BUDGET_LIST_ID_KEY)) || []
+console.log(budgetList);
 
+// When an item on the side bar is clicked on, direct the user to that tab
 sidebar.addEventListener('click', e => {
   if (e.target.tagName.toLowerCase() === 'button') {
       selectedTabId = e.target.id   
@@ -13,11 +35,88 @@ sidebar.addEventListener('click', e => {
   saveAndRender()
 })
 
+// When a form for new budget category is added, display that on the UI
+newBudgetForm.addEventListener("submit", e => {
+  e.preventDefault()  
+  const categoryName = newBudgetCategory.value
+  const budgetAmount = newBudgetAmount.value  
 
-function save() {
-  localStorage.setItem(LOCAL_STORAGE_SELECTED_TAB_ID_KEY, selectedTabId)
+  if (categoryName == null || categoryName === '') return
+  if (budgetAmount == null || budgetAmount === '') return
+
+  const budget = createBudget(categoryName, budgetAmount)
+
+  newBudgetCategory.value = null
+  newBudgetAmount.value = null
+
+  budgetList.push(budget)
+
+  saveAndRenderBudgets()
+  renderBudgets()
+})
+
+newItemForm.addEventListener("submit", e => {
+  e.preventDefault()
+  const itemDescription = newDescription.value
+  const itemPrice = newPrice.value
+  const categoryName = categoryOptions.value
+
+  if (itemDescription == null || itemDescription === '') return 
+  if (itemPrice == null || itemPrice === '') return 
+  if (categoryName == null) return
+
+  newDescription.value = null
+  newPrice.value = null
+  categoryOptions.value = null
+
+  const item = createItem(itemDescription, itemPrice)
+  let index = 0
+  budgetList.forEach(category => {
+    if (category.name == categoryName)
+        index = budgetList.indexOf(category)
+  })
+
+  budgetList[index].items.push(item)
+
+  setSelectedOption()
+  save()
+  renderItems()
+})
+
+// Create a new object in the budget list. Object has a name, amount, and list of spent items
+function createBudget(category , amount) {
+  return {
+    name: category,
+    budget: amount,
+    items: []
+  }
 }
 
+function createItem(description, amount) {
+  return {
+    title: description,
+    price: amount
+  }
+}
+
+// Render everything and display onto the UI
+renderBudgets()
+render()
+
+// Save data every time the user input new things 
+function save() {
+  localStorage.setItem(LOCAL_STORAGE_SELECTED_TAB_ID_KEY, selectedTabId)
+  localStorage.setItem(LOCAL_STORAGE_BUDGET_LIST_ID_KEY, JSON.stringify(budgetList))
+}
+
+// clear all elements inside a list
+function clearElements(element) {
+  while (element.firstChild) {
+    element.removeChild(element.firstChild)
+  }
+}
+
+// render and display
 function render() {
   sidebarList.forEach(tab => {
     const pageId = 'money-' + tab.id
@@ -34,9 +133,56 @@ function render() {
   });
 }
 
+// render the planned budgets tab
+function renderBudgets() {
+  clearElements(budgetContainer)
+  clearElements(selectBox)
+  budgetList.forEach(category => {
+      const categoryElement = document.importNode(budgetTemplate.content, true)
+      const categoryTitle = categoryElement.querySelector(".card-title b")
+      categoryTitle.append(category.name)
+      const budget = categoryElement.getElementById("budget-amount") 
+      budget.append(category.budget)
+      budgetContainer.prepend(categoryElement)
+
+      const option = document.createElement('option')
+      option.text = category.name
+      selectBox.appendChild(option)
+  });
+
+  renderItems()
+}
+
+function setSelectedOption() {
+  const option = selectBox.firstChild
+  option.selected = true
+}
+
+function renderItems() {
+  clearElements(itemsContainer)
+  budgetList.forEach(category => {
+    const itemsList = category.items
+    itemsList.forEach(item => {
+        const itemElement = document.importNode(itemTemplate.content, true)
+        const itemTitle = itemElement.querySelector(".card-title b")
+        itemTitle.append(item.title)
+        const categoryName = itemElement.querySelector("#spend-type small")
+        categoryName.append(category.name)
+        const price = itemElement.getElementById("spend-amount")
+        price.append(item.price)
+
+        itemsContainer.prepend(itemElement)
+    })
+  });
+}
+
+
 function saveAndRender() {
   save()
   render()
 }
 
-saveAndRender()
+function saveAndRenderBudgets() {
+  save()
+  renderBudgets()
+}
